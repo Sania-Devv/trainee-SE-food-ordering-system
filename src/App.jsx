51f1d7277ce56f74ae6abc7ee80380b1a518b81c
Sania-Dev <hafizasania6@gmail.com>
2026-07-07 16:36:15 +0500

    login/signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./pages/customer/Home";
@@ -8,22 +6,34 @@ import Offers from "./pages/customer/Offers";
 import RestaurantDetail from "./pages/customer/RestaurantDetail";
 import OrderTracking from "./pages/customer/OrderTracking";
 import Login from "./pages/customer/Login";
+import Signup from "./pages/customer/Signup";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import AuthLayout from "./components/common/AuthLayout"
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <Routes>
-        <Route element={<Navbar/>}>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<MenuItemDetail />} />
-          <Route path="/offers" element={<Offers />} />
-          <Route path="/restaurants" element={<RestaurantDetail />} />
-          <Route path="/track-order" element={<OrderTracking />} />
-          <Route path="/login" element={<Login />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route element={<Navbar />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<MenuItemDetail />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/restaurants" element={<RestaurantDetail />} />
+
+        <Route
+          path="/track-order"
+          element={
+            <ProtectedRoute>
+              <OrderTracking />
+            </ProtectedRoute>
+          }
+        />
+
+      
+      </Route>
+        <Route element={<AuthLayout />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Route>
+    </Routes>
   );
 }
 
